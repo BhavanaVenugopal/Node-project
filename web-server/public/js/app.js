adb39e4f8fdf236b7b86fc5439b1761bbecf281d
Bhavana V <bhavana.kavi@gmail.com>
2022-04-25 22:00:54 +0530

    practice 2

---
+++ web-server/public/js/app.js
@@ -0,0 +1,18 @@
+console.log("JS loaded")
+
+const api_url = "http://localhost:3000/weather?address="
+
+const weatherForm = document.querySelector("form")
+const search = document.querySelector("input")
+
+weatherForm.addEventListener("submit", (event) => {
+    event.preventDefault()
+    const address = search.value
+    fetch(api_url + address)
+    .then((response)=>{
+        response.json().then((data) => {
+            console.log(data)
+            document.getElementById("weather-update").innerHTML = data.forecast
+        })
+    })
+})
